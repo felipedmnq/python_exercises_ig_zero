f45f5b21c6508bd4f22b832c9bc13b106fabafc7
Felipe Demenech Vasconcelos <felipedmnq@gmail.com>
2020-09-20 06:54:18 -0300

    Test, update GitHub directly from pycharm.

--- ex007_ATM_cash.py
+++ ex007_ATM_cash.py
@@ -7,24 +7,24 @@ O valor mínimo é R$10, maximo R$600
 O programa não deve se preocupar com as notas existentes na maquina.
 '''
 msg = '      ATM do Felipe      '
-print('-' * len(msg))
+print('\033[1;34m-' * len(msg))
 print(msg)
 print('-' * len(msg))
 print('Mínimo R$ 10,00\nMáximo R$ 600,00')
 print('-' * len(msg))
 while True:
-    value = int(input('Valor do saque: R$ '))
+    value = int(input('\033[1;33mValor do saque: R$ \033[m'))
     if value < 10 or value > 600:
-        print('VALOR INVÁLIDO.')
+        print('\033[1;31mVALOR INVÁLIDO.\033[m')
     else:
         break
-print(f'O valor solicitado foi R$ {value},00')
-
+print(f'\033[1;31mO valor solicitado foi R$ {value},00')
+print('\033[1;34m-\033[m' * len(msg))
 tot = value
 ced = value // 100
 rest = value % 100
 if ced > 0:
-    print(f'Total de {ced} cédulas de R$ 100,00.')
+    print(f'\033[1;32mTotal de {ced} cédulas de R$ 100,00.')
 ced = rest // 50
 rest %= 50
 if ced > 0:
