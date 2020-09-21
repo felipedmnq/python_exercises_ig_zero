d6f32c6c1ca2e2723831218b1b24f7c0c03a5861
Felipe Demenech Vasconcelos <felipedmnq@gmail.com>
2020-09-21 19:38:45 -0300

    ex014 - while.

--- ex015_odd_numbers.py
+++ ex015_odd_numbers.py
@@ -1,3 +1,10 @@
 '''
 Dado um número inteiro positivo n, imprimir os n primeiros numeoros naturais impares.
 '''
+
+num = int(input('Digite um número inteiro positivo: '))
+cont = 0
+while cont <= num:
+    if cont % 2 == 1:
+        print(cont, end=' ')
+    cont += 1
