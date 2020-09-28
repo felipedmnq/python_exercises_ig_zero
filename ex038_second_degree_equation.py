b8e46c09c84ae99cdf0d8a85d1807aaf395953fb
Felipe Demenech Vasconcelos <felipedmnq@gmail.com>
2020-09-28 16:55:38 -0300

    class 20 - float variables.

--- ex038_second_degree_equation.py
+++ ex038_second_degree_equation.py
@@ -11,15 +11,15 @@ informar:
     raiz = (-b +- (delta)**1/2) (2*a)
 '''
 
-a = int(input('Informe o valor de A: '))
+a = float(input('Informe o valor de A: '))
 if a == 0:
     print(f'A variável A sendo 0, não caracteriza uma equação de 2º grau.')
 else:
-    b = int(input('Informe o valor de B: '))
-    c = int(input('Informe o valor de C: '))
+    b = float(input('Informe o valor de B: '))
+    c = float(input('Informe o valor de C: '))
     delta = b ** 2 - 4 * a * c
-    raiz_pos = (-b + (b ** 2 - 4 * a * c) ** 1 / 2) / (2 * a)
-    raiz_neg = (-b - (b ** 2 - 4 * a * c) ** 1 / 2) / (2 * a)
+    raiz_pos = float(-b + (delta) ** 1 / 2) / (2 * a)
+    raiz_neg = float(-b - (delta) ** 1 / 2) / (2 * a)
     if delta < 0:
         print('Delta possui valor negativo, por esse motivo a equação não possui raizes reais.')
     elif delta == 0:
--- testes.py
+++ testes.py
@@ -1,6 +1,2 @@
-n = int(input('N: '))
-soma = 0.0
-
-for i in range(1, n+ 1):
-    soma += i / (n - i + 1)
-print(f'A soma vale: {soma}.')
+a = (-2 - (2 ** 2 - 4 * 2 * (-1)) ** 1 / 2) / (2 * 2)
+print(a)
