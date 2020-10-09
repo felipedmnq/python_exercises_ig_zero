e4d8d3982419608a7ccd294cc2bb3ee91259d249
Felipe Demenech Vasconcelos <felipedmnq@gmail.com>
2020-10-09 20:03:20 -0300

    class 30 - attack/cure game - exercises.

--- ex055_notes.py
+++ ex055_notes.py
@@ -10,3 +10,38 @@ imprima:
     7 - qtidade de valores abaixo de 7
     8 - encerre o programa com uma mensagem.
 '''
+notes = []
+
+while True:
+    note = float(input('Nota ([-1] para sair): '))
+    if note == -1:
+        break
+    notes.append(note)
+
+print('-' * 30)
+print(f'Foram cadastradas {len(notes)} notas.')
+print('As notas cadastradas foram: ', end='')
+for nota in notes:
+    print(nota, end=', ')
+
+print()
+print('Ordem invertida: ', end='')
+for nota in reversed(notes):
+    print(nota, end=', ')
+
+print()
+print(f'A soma das notas digitadas é: {sum(notes)}.'
+      f'\nA média das notas digitas foi: {sum(notes) / len(notes)}.')
+
+upper_qtd = 0
+for nota in notes:
+    if nota > (sum(notes) / len(notes)):
+        upper_qtd += 1
+print(f'Foram digitadas {upper_qtd} notas acima da média.')
+
+lower_qtd = 0
+for nota in notes:
+    if nota < 7:
+        lower_qtd += 1
+print(f'Foram digitadas {lower_qtd} notas abaixo de 7.')
+print('FUI....VALEU!!')
