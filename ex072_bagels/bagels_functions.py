50382c0fd639534f4708f20a8b9d3bc85683e9c6
Felipe Demenech Vasconcelos <felipedmnq@gmail.com>
2020-10-16 07:17:12 -0300

    class 43 - Bagels Game.

--- ex072_bagels/bagels_functions.py
+++ ex072_bagels/bagels_functions.py
@@ -1,5 +1,7 @@
 import random
 
+ult_dig = (lambda x: x%10)
+
 
 def VerificaEntrada(num):
     """
@@ -8,6 +10,10 @@ def VerificaEntrada(num):
     True --> Entrada Válida
     False --> Entrada Inválida
     """
+    if 1000 <= num < 10000:
+        return True
+    else:
+        return False
 
 
 
@@ -20,6 +26,17 @@ def GeraSecretNum():
     list_num = [1,7,8,3]
     OBS: O NUMERO NAO PODE TER DIGITOS REPETIDOS
     """
+    numbers = list(range(10))
+    secretNum = 0
+
+    while numbers[0] == 0:
+        random.shuffle(numbers)
+
+    for i in range(4):
+        digit = numbers[i]
+        secretNum = digit * (10 ** (3 - i))
+
+    return secretNum, numbers[:4]
 
 
 def GeraDicas(num, secretNum, secretNumList):
@@ -34,4 +51,24 @@ def GeraDicas(num, secretNum, secretNumList):
 
     Retorna uma lista vazia caso os dois números sejam iguais
     """
+    global ult_dig
+    if secretNum == num:
+        return []
 
+    dica = []
+
+    for i in range(4):
+        if ult_dig(num) == ult_dig(secretNum):
+            dica.append(2)
+        elif ult_dig(num) in secretNumList:
+            dica.append(1)
+
+        num //= 10
+        secretNum //= 10
+
+    if len(dica) == 0:
+        dica.append(0)
+
+    dica.sort()
+
+    return dica
