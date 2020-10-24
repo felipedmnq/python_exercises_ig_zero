52db6e3a65f618f186a94fda13b06c035ccbfed9
Felipe Demenech Vasconcelos <felipedmnq@gmail.com>
2020-10-24 08:28:51 -0300

    class 51 - Python file management - open/write/read/append. ex090 - report generator.

--- python_files_classs/ex090_users_report.py
+++ python_files_classs/ex090_users_report.py
@@ -38,3 +38,37 @@ através de uma função separada, que será chamada pelo programa principal.
 O cálculo do percentual de uso também deverá ser feito através de uma função,
 que será chamada pelo programa principal.
 """
+
+def extract_bytes(file = ''):
+    file_open = open(f'{file}', 'r')
+    extract = file_open.readlines()
+    extracted = []
+    for c in range(len(extract)):
+        extracted.append(int(extract[c][15:]))
+
+    return extracted
+
+
+def report_generator(file):
+    extracted = extract_bytes(file='users.txt')
+    extracted_sum = sum(extracted)
+    extract = file.readlines()
+    for c in range(len(extract)):
+        print(f'{c + 1:<4}\t{extract[c][:15]:<15}{(extracted[c]) / (1024 ** 2):>15.2f} MB'
+              f'\t\t\t{(extracted[c] * 100) / extracted_sum:>5.2f}%')
+
+
+def main(file):
+    user = open(file, 'r')
+    detail = ('-' * 60)
+    print(detail)
+    print(f'{"ACME Inc.  - Uso do espaço em disco por usuário.":^60}')
+    print(detail)
+    print(f'{"No.":<4}\t{"Usuário":<15}\t\t{"Espaço utilizado":^15}\t\t{"% Uso":>}')
+
+    report_generator(user)
+
+    print(detail)
+
+
+main('users.txt')
